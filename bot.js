950f81c1b9fd637afcc9c997385169c9c3bb0671
Blanksoon <farm1771@gmail.com>
2017-06-22 17:19:32 +0700

    edit

--- bot.js
+++ bot.js
@@ -1,26 +1,36 @@
-var express = require('express')
-var bodyParser = require('body-parser')
-var request = require('request')
-var app = express()
+var app = require('express')();
+//var bodyParser = require('body-parser')
+//var request = require('request')
+//var app = express()
+var port = process.env.PORT || 7777;
+//app.use(bodyParser.json())
 
-app.use(bodyParser.json())
+//app.set('port', (process.env.PORT || 4000))
+//app.use(bodyParser.urlencoded({extended: true}))
+//app.use(bodyParser.json())
 
-app.set('port', (process.env.PORT || 4000))
-app.use(bodyParser.urlencoded({extended: true}))
-app.use(bodyParser.json())
-
-app.post('/', (req, res) => {
-  var text = req.body.events[0].message.text
+app.get('/', (req, res) => {
+  var text = req.body.events[0].message.text 
   var sender = req.body.events[0].source.userId
   var replyToken = req.body.events[0].replyToken
-  console.log(text, sender, replyToken)
-  console.log(typeof sender, typeof text)
-  // console.log(req.body.events[0])
   if (text === 'สวัสดี' || text === 'Hello' || text === 'hello') {
-    sendText(sender, text)
+     sendText(sender, text)
   }
   res.sendStatus(200)
-})
+  //res.send('<h1>Hello Node.js</h1>');
+});
+//   console.log('in')
+//   var text = req.body.events[0].message.text
+//   var sender = req.body.events[0].source.userId
+//   var replyToken = req.body.events[0].replyToken
+//   console.log(text, sender, replyToken)
+//   console.log(typeof sender, typeof text)
+//   // console.log(req.body.events[0])
+//   if (text === 'สวัสดี' || text === 'Hello' || text === 'hello') {
+//     sendText(sender, text)
+//   }
+//   res.sendStatus(200)
+// })
 
 function sendText (sender, text) {
   let data = {
@@ -48,6 +58,6 @@ function sendText (sender, text) {
   })
 }
 
-app.listen(app.get('port'), function () {
+app.listen(port, function () {
   console.log('run at port', app.get('port'))
 })

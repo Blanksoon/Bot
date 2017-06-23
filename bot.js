6b2b35610bc8ec7ec816c3b0636c2b9d8d82e47b
Blanksoon <farm1771@gmail.com>
2017-06-23 13:18:30 +0700

    test

--- bot.js
+++ bot.js
@@ -7,10 +7,10 @@ app.set('port', (process.env.PORT || 4000))
 //app.use(bodyParser.urlencoded({extended: true}))
 app.use(bodyParser.json())
 
-app.post('/webhook', (req, res) => {
-  var text = req.body.events[0].message.text
-  var sender = req.body.events[0].source.userId
-  var replyToken = req.body.events[0].replyToken
+app.post('/', (req, res) => {
+  var text = req.body.events.message.text
+  var sender = req.body.events.source.userId
+  var replyToken = req.body.events.replyToken
   console.log(text, sender, replyToken)
   console.log(typeof sender, typeof text)
   // console.log(req.body.events[0])

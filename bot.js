726cfe982600adc81cbf8d44796d721db24f061c
Blanksoon <farm1771@gmail.com>
2017-06-23 13:25:13 +0700

    test

--- bot.js
+++ bot.js
@@ -7,15 +7,15 @@ app.set('port', (process.env.PORT || 4000))
 //app.use(bodyParser.urlencoded({extended: true}))
 app.use(bodyParser.json())
 app.get('/', (req, res) => {
-  var text = req.body.events.message.text
-  var sender = req.body.events.source.userId
-  var replyToken = req.body.events.replyToken
-  console.log(text, sender, replyToken)
-  console.log(typeof sender, typeof text)
+  // var text = req.body.events.message.text
+  // var sender = req.body.events.source.userId
+  // var replyToken = req.body.events.replyToken
+  // console.log(text, sender, replyToken)
+  // console.log(typeof sender, typeof text)
   // console.log(req.body.events[0])
-  if (text === 'สวัสดี' || text === 'Hello' || text === 'hello') {
-    sendText(sender, text)
-  }
+  // if (text === 'สวัสดี' || text === 'Hello' || text === 'hello') {
+  //   sendText(sender, text)
+  // }
   res.sendStatus(200)
 })
 
